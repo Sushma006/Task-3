b9244ef3a34c504e6bda12138a4c1b991625eb00
Mopuri Sushma <mopurisushmareddy@gmail.com>
2026-06-01 19:31:58 +0530

    Create register4.v

---
+++ register4.v
@@ -0,0 +1,73 @@
+//Verilog
+module register4(
+    input clk,
+    input [3:0] D,
+    output reg [3:0] Q
+);
+
+always @(posedge clk)
+begin
+    Q <= D;
+end
+
+endmodule
+
+//Testbench
+module tb_register4;
+
+reg clk;
+reg [3:0] D;
+wire [3:0] Q;
+
+register4 uut(
+    .clk(clk),
+    .D(D),
+    .Q(Q)
+);
+
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_register4);
+end
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+
+    D = 4'b0000;
+    #10;
+
+    D = 4'b1010;
+    #10;
+
+    D = 4'b1100;
+    #10;
+
+    D = 4'b1111;
+    #10;
+
+    D = 4'b0011;
+    #10;
+
+    $finish;
+end
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
