fcabf52ff76a20f78dcfd1057d86c57e245cf5ca
Mopuri Sushma <mopurisushmareddy@gmail.com>
2026-06-01 19:42:01 +0530

    Delete testbench from counter4.v
    Removed testbench code for the counter4 module.

--- counter4.v
+++ counter4.v
@@ -14,30 +14,3 @@ initial begin
 end
 
 endmodule
-
-//Testbench
-module tb_counter4;
-
-reg clk;
-wire [3:0] count;
-
-counter4 uut(
-    .clk(clk),
-    .count(count)
-);
-
-initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(0, tb_counter4);
-end
-
-always #5 clk = ~clk;
-
-initial begin
-    clk = 0;
-
-    #100;
-    $finish;
-end
-
-endmodule
