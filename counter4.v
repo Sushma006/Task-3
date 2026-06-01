5413785b8d2d96a4a863caa22744ad60d4da90ae
Mopuri Sushma <mopurisushmareddy@gmail.com>
2026-06-01 19:34:21 +0530

    Add 4-bit counter and testbench in Verilog

---
+++ counter4.v
@@ -0,0 +1,43 @@
+//Veriolg
+module counter4(
+    input clk,
+    output reg [3:0] count
+);
+
+always @(posedge clk)
+begin
+    count <= count + 1;
+end
+
+initial begin
+    count = 4'b0000;
+end
+
+endmodule
+
+//Testbench
+module tb_counter4;
+
+reg clk;
+wire [3:0] count;
+
+counter4 uut(
+    .clk(clk),
+    .count(count)
+);
+
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_counter4);
+end
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+
+    #100;
+    $finish;
+end
+
+endmodule
