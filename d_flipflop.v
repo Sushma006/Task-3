fb6b76dad39992be2be55c94f56e2280e1f46da0
Mopuri Sushma <mopurisushmareddy@gmail.com>
2026-06-01 19:38:43 +0530

    Delete testbench for d_flipflop
    Removed the testbench for the D flip-flop module.

--- d_flipflop.v
+++ d_flipflop.v
@@ -13,35 +13,3 @@ end
 endmodule
 
 
-//Testbench
-module tb_d_flipflop;
-
-reg clk;
-reg D;
-wire Q;
-
-d_flipflop uut(
-    .clk(clk),
-    .D(D),
-    .Q(Q)
-);
-
-initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars(0, tb_d_flipflop);
-end
-
-always #5 clk = ~clk;
-
-initial begin
-    clk = 0;
-    D = 0;
-
-    #10 D = 1;
-    #10 D = 0;
-    #10 D = 1;
-
-    #20 $finish;
-end
-
-endmodule
