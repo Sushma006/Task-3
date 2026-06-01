5aa4c81be3f4af86e886e0593222c5a162fabf43
Mopuri Sushma <mopurisushmareddy@gmail.com>
2026-06-01 19:23:14 +0530

    Add D flip-flop module and testbench
    Implemented a D flip-flop module with a testbench.

---
+++ d_flipflop.v
@@ -0,0 +1,47 @@
+//verilog code
+module d_flipflop(
+    input clk,
+    input D,
+    output reg Q
+);
+
+always @(posedge clk)
+begin
+    Q <= D;
+end
+  
+endmodule
+
+
+//Testbench
+module tb_d_flipflop;
+
+reg clk;
+reg D;
+wire Q;
+
+d_flipflop uut(
+    .clk(clk),
+    .D(D),
+    .Q(Q)
+);
+
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_d_flipflop);
+end
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+    D = 0;
+
+    #10 D = 1;
+    #10 D = 0;
+    #10 D = 1;
+
+    #20 $finish;
+end
+
+endmodule
